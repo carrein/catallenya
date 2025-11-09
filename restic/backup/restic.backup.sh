c8fe374f8cc5fc540c3198b87d3cc99039fd4821
carrein <carreinlee@proton.me>
2025-11-09 03:37:53 -0800

    update systemctl.

--- restic/backup/restic.backup.sh
+++ restic/backup/restic.backup.sh
@@ -3,4 +3,7 @@
 . /zpool/catallenya/restic/restic.conf
 
 # Backup target folders to repository.
-restic -r ${RESTIC_DRIVER}:${RESTIC_RCLONE_REMOTE}:${RESTIC_BACKUP_LOCATION} --verbose backup ${RESTIC_BACKUP_TARGETS} --password-file ${RESTIC_PASSWORD_FILE}
+restic -r ${RESTIC_DRIVER}:${RESTIC_RCLONE_REMOTE}:${RESTIC_BACKUP_LOCATION} \
+    --exclude ${RESTIC_EXCLUDE_TARGETS} \
+    --verbose backup ${RESTIC_BACKUP_TARGETS} \
+    --password-file ${RESTIC_PASSWORD_FILE}
--- restic/check/restic.check.sh
+++ restic/check/restic.check.sh
@@ -2,5 +2,21 @@
 
 . /zpool/catallenya/restic/restic.conf
 
-# Verify integrity of repository.
-restic -r ${RESTIC_DRIVER}:${RESTIC_RCLONE_REMOTE}:${RESTIC_BACKUP_LOCATION} --verbose check --read-data --password-file ${RESTIC_PASSWORD_FILE}
+check_type="$1"
+
+case "$check_type" in
+    meta)
+        restic -r ${RESTIC_DRIVER}:${RESTIC_RCLONE_REMOTE}:${RESTIC_BACKUP_LOCATION} \
+            --verbose check \
+            --password-file ${RESTIC_PASSWORD_FILE}
+        ;;
+    data)
+        restic -r ${RESTIC_DRIVER}:${RESTIC_RCLONE_REMOTE}:${RESTIC_BACKUP_LOCATION} \
+            --verbose check --read-data \
+            --password-file ${RESTIC_PASSWORD_FILE}
+        ;;
+    *)
+        echo "Unknown check type: $check_type" >&2
+        exit 1
+        ;;
+esac
